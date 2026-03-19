db5aa2b0e721d47161684dc1fa9a8afdba655591
C-Ewan <atukdibe@gmail.com>
2026-03-18 23:29:06 -0300

    refactor: streamline VPS deployment script to use tar for file transfer and improve backup handling

--- scripts/deploy_to_vps.sh
+++ scripts/deploy_to_vps.sh
@@ -23,28 +23,23 @@ docker save -o ../docker/ania_web_admin.tar ania_web_admin
 docker tag ania_s3_garage:latest ania_s3_garage
 docker save -o ../docker/ania_s3_garage.tar ania_s3_garage
 
-# Copy docker folder to VPS
-scp -P "$VPS_PORT"               \
-    ../docker/.env               \
+tar czvf - \
+    ../docker/.env \
     ../docker/docker-compose.yml \
-    ../docker/ania_nginx.tar     \
-    ../docker/ania_web.tar       \
-    ../docker/ania_server.tar    \
+    ../docker/ania_nginx.tar \
+    ../docker/ania_web.tar \
+    ../docker/ania_server.tar \
     ../docker/ania_web_admin.tar \
     ../docker/ania_s3_garage.tar \
-    "$VPS_USER@$VPS_HOST:$REMOTE_PATH/docker"
+    ../scripts/backup.sh \
+| ssh -p "$VPS_PORT" "$VPS_USER@$VPS_HOST" "
+set -e
 
-# Remove docker images
-rm -f ../docker/ania_nginx.tar      \
-      ../docker/ania_web.tar        \
-      ../docker/ania_server.tar     \
-      ../docker/ania_web_admin.tar  \
-      ../docker/ania_s3_garage.tar
+mkdir -p '$REMOTE_PATH/docker' '$REMOTE_PATH/scripts'
+tar xzf - -C '$REMOTE_PATH'
 
-# Load images and restart containers
-
-ssh -p "$VPS_PORT" "$VPS_USER@$VPS_HOST" <<EOF
-cd "$REMOTE_PATH/docker"
+chmod +x '$REMOTE_PATH/scripts/backup.sh'
+cd '$REMOTE_PATH/docker'
 docker compose down -v --remove-orphans
 docker load -i ania_nginx.tar 
 docker load -i ania_web.tar
@@ -52,4 +47,11 @@ docker load -i ania_server.tar
 docker load -i ania_web_admin.tar
 docker load -i ania_s3_garage.tar
 docker compose up -d
-EOF
+"
+
+# Remove docker images
+rm -f ../docker/ania_nginx.tar      \
+      ../docker/ania_web.tar        \
+      ../docker/ania_server.tar     \
+      ../docker/ania_web_admin.tar  \
+      ../docker/ania_s3_garage.tar
